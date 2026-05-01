cc19f47659324209b33797663d7c4ee05bb770b0
Azim Kazi <azzimkazzi@gmail.com>
2026-05-01 08:06:17 +0530

    changes

---
+++ JivanJyotiTrustApp/Migrations/20260501021755_TrainingModuleModified.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JivanJyotiTrustApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrainingModuleModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Trainings",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Trainings");
+        }
+    }
+}
