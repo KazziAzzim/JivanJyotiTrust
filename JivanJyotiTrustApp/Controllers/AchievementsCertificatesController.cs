50856a40825f423565121e122553dc5887c09cb0
KazziAzzim <38529205+KazziAzzim@users.noreply.github.com>
2026-05-07 11:46:24 +0530

    Enhance achievements certificates listing

---
+++ JivanJyotiTrustApp/Controllers/AchievementsCertificatesController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace JivanJyotiTrustApp.Controllers;
+
+public class AchievementsCertificatesController : Controller
+{
+    public IActionResult Index() => View();
+}
